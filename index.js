29adaa83d41471ba9e5c2c9d783fce6b109d7cfc
“sotirios.moustogiannis” <smoustogiannis@konnektable.com>
2023-09-05 02:09:20 +0300

    change db url

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
-//process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
 
 const campgroundsRoutes = require('./routes/campgrounds');
 const reviewsRoutes = require('./routes/reviews');
